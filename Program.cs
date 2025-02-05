fb49b38a1841be474a214ef22f1c5d3bd365b768
Danilkaaa323 <danilkaaa323@gmail.com>
2025-02-05 16:48:08 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace training_pattern_prototip
+{
+    public abstract class Shape
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Color { get; set; }
+        public Shape(int x, int y, string color)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+        }
+        protected Shape(Shape source)
+        {
+            if (source != null)
+            {
+                X = source.X;
+                Y = source.Y;
+                Color = source.Color;
+            }
+        }
+        public abstract Shape Clone();
+    }
+    public class Rectangle : Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Rectangle(int x, int y, string color, int width, int height) : base(x, y, color)
+        {
+            Width = width;
+            Height = height;
+        }
+        protected Rectangle(Rectangle source) : base(source)
+        {
+            Width = source.Width;
+            Height = source.Height;
+        }
+        public override Shape Clone() => new Rectangle(this);
+    }
+    public class Circle : Shape
+    {
+        public int Radius { get; set; }
+        public Circle(int x, int y, string color, int radius) : base(x, y, color)
+        {
+            Radius = radius;
+        }
+        protected Circle(Circle source) : base(source)
+        {
+            Radius = source.Radius;
+        }
+        public override Shape Clone() => new Circle(this);
+    }
+    public class Cube : Shape
+    {
+        public int Side { get; set; }
+        public Cube(int x, int y, string color, int side) : base(x, y, color)
+        {
+            Side = side;
+        }
+        protected Cube(Cube source) : base(source)
+        {
+            Side = source.Side;
+        }
+        public override Shape Clone() => new Cube(this);
+    }
+    public class Sphere : Shape
+    {
+        public int Radius { get; set; }
+        public Sphere(int x, int y, string color, int radius) : base(x, y, color)
+        {
+            Radius = radius;
+        }
+        protected Sphere(Sphere source) : base(source)
+        {
+            Radius = source.Radius;
+        }
+        public override Shape Clone() => new Sphere(this);
+    }
+    public class Application
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("training_pattern_prototip")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("training_pattern_prototip")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9c344a55-4e18-4b94-a43f-a02aef0e664f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
